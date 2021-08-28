f4980fffe568b4c405fb7b746723a26e365d82b9
trey368s <84042739+trey368s@users.noreply.github.com>
2021-08-28 13:00:44 -0400

    Percent sizer

--- backtraderTest.py
+++ backtraderTest.py
@@ -1,22 +1,17 @@
 import backtrader
 import datetime
 from strategies import TestStrategy
+data = backtrader.feeds.YahooFinanceCSVData(
+    dataname='historicalData/BTC2021.csv',
+    fromdate=datetime.datetime(2000, 1, 1),
+    todate=datetime.datetime(2021, 12, 31), reverse=False)
 
 cerebro = backtrader.Cerebro()
 cerebro.broker.set_cash(1000000)
-# Create a Data Feed
-data = backtrader.feeds.YahooFinanceCSVData(
-    dataname='historicalData/BTC2021.csv',
-        # Do not pass values before this date
-    fromdate=datetime.datetime(2000, 1, 1),
-        # Do not pass values after this date
-    todate=datetime.datetime(2021, 12, 31),
-        reverse=False)
-
 cerebro.adddata(data)
 cerebro.addstrategy(TestStrategy)
-cerebro.addsizer(backtrader.sizers.FixedSize, stake=20)
+cerebro.addsizer(backtrader.sizers.PercentSizer, percents = 99)
 print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
 cerebro.run()
 print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
-cerebro.plot(style="candlestick")  
+cerebro.plot(style="candlestick", barup='lawngreen', bardown='red')  
--- strategies.py
+++ strategies.py
@@ -3,18 +3,17 @@ import backtrader
 class TestStrategy(backtrader.Strategy):
 
     def log(self, txt, dt=None):
-        ''' Logging function fot this strategy'''
         dt = dt or self.datas[0].datetime.date(0)
         print('%s, %s' % (dt.isoformat(), txt))
 
     def __init__(self):
-        # Keep a reference to the "close" line in the data[0] dataseries
         self.dataclose = self.datas[0].close
         self.order = None
         self.macd = backtrader.indicators.MACDHisto(self.data)
         #self.rsi = backtrader.indicators.RelativeStrengthIndex(self.data)
-        #self.bBands = backtrader.indicators.BollingerBands(self.data)
-        self.ema = backtrader.indicators.ExponentialMovingAverage(self.data, period=21)
+        #self.bbands = backtrader.indicators.BollingerBands(self.data)
+        #self.psar = backtrader.indicators.ParabolicSAR(self.data)
+        self.ema = backtrader.indicators.ExponentialMovingAverage(self.data, period = 21)
 
     def notify_order(self, order):
         if order.status in [order.Submitted, order.Accepted]:
@@ -30,18 +29,15 @@ class TestStrategy(backtrader.Strategy):
         self.order = None
 
     def next(self):
-        # Simply log the closing price of the series from the reference
         self.log('Close, %.2f' % self.dataclose[0])
 
         if self.order:
             return
         if not self.position:
             if self.macd.macd[0] < self.macd.signal[0] and self.ema[0] > self.data[0]:
-                    # previous close less than the previous close
-                    # BUY, BUY, BUY!!! (with all possible default parameters)
                     self.log('BUY CREATED, %.2f' % self.dataclose[0])
                     self.order = self.buy()
-        else:
+        if self.position:
             if self.macd.macd[0] > self.macd.signal[0] and self.ema[0] < self.data[0]:
                 self.log('SELL CREATED {}'.format(self.dataclose[0]))
                 self.order = self.sell()
